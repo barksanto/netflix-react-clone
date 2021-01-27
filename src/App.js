cc51fd7b32e4c52727cda20baeafcb7c6f8bfce4
Barkley Santo <barkley.santo@gmail.com>
2021-01-27 13:27:00 -0500

    added api key and requests variable with interpolated key

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Let's build a Netflix Clone!</h1>
+      <h1>Let's build a Netflix Clone!🔥</h1>
     </div>
   );
 }
