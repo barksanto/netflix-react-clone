8f8e6f04665760fdb10f486e08ce891f5087fe6f
Barkley Santo <barkley.santo@gmail.com>
2021-01-28 13:14:14 -0500

    render remaining categories into Row component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ function App() {
       <h1>Netflix Clone!🔥</h1>
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.TopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
     </div>
   );
 }
