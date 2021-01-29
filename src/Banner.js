144ed045a84f141d16ccdf1811fc27d8b217ec66
Barkley Santo <barkley.santo@gmail.com>
2021-01-29 14:38:58 -0500

    styling buttons - some opacity so we can see the text no matter the color of bg image

--- src/Banner.js
+++ src/Banner.js
@@ -25,18 +25,23 @@ function Banner() {
     backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
     backgroundPosition: "center center"
   }
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     < header className="banner" style={bannerStyle}>
       <div className="banner-contents">
         {/* title */}
-        <h1>{movie?.name || movie?.title || movie?.original_name}</h1>
-        <h1 className="banner-description">{movie.overview}</h1>
+        <h1 className="banner-title">{movie?.name || movie?.title || movie?.original_name}</h1>
         {/* div with 2 buttons play+my list */}
         <div className="banner-buttons">
           <button className="banner-button">Play</button>
           <button className="banner-button">My List</button>
         </div>
         {/* description */}
+        <h1 className="banner-description">{truncate(movie.overview, 150)}</h1>
 
       </div>
 
