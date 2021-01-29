1788fdab46462289bc93ed41023bbf8a6394da70
Barkley Santo <barkley.santo@gmail.com>
2021-01-29 12:22:36 -0500

    import useEffect and useState for banner component - fetching one movie for banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from './axios';
+import requests from './requests'
 
 function Banner() {
+  // this is responsible for every random movie set at the top
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      //banner build
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie)
+
   return (
-    <header>
+    <header> {/* This header will have a background image */}
       {/* title */}
+      {/* div with 2 buttons play+my list */}
+      {/* description */}
+
     </header>
   )
 }
