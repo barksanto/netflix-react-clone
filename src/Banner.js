49d3af32f035b481ca9a6beda49af1e20b6c7267
Barkley Santo <barkley.santo@gmail.com>
2021-01-29 13:50:14 -0500

    added height to banner image and 2 buttons - play and My List

--- src/Banner.js
+++ src/Banner.js
@@ -18,18 +18,22 @@ function Banner() {
     fetchData();
   }, []);
 
-  console.log(movie)
+  // console.log(movie);
   const bannerStyle = {
     backgroundSize: "cover",
     backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
     backgroundPosition: "center center",
-    height: "20vh"
+    height: '200px'
   }
   return (
     < header className="banner" style={bannerStyle}>
       <div className="banner-contents">
         {/* title */}
         <h1>{movie?.name || movie?.title || movie?.original_name}</h1>
+        <div className="banner-buttons">
+          <button className="banner-button">Play</button>
+          <button className="banner-button">My List</button>
+        </div>
         {/* div with 2 buttons play+my list */}
         {/* description */}
       </div>
