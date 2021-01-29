25fdd674059528d3794ca8da82209aa1d533b885
Barkley Santo <barkley.santo@gmail.com>
2021-01-29 14:23:42 -0500

    set proper height and color for text in the banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
 import requests from './requests'
+import './Banner.css';
 
 function Banner() {
   // this is responsible for every random movie set at the top
@@ -22,20 +23,21 @@ function Banner() {
   const bannerStyle = {
     backgroundSize: "cover",
     backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-    backgroundPosition: "center center",
-    height: '200px'
+    backgroundPosition: "center center"
   }
   return (
     < header className="banner" style={bannerStyle}>
       <div className="banner-contents">
         {/* title */}
         <h1>{movie?.name || movie?.title || movie?.original_name}</h1>
+        <h1 className="banner-description">{movie.overview}</h1>
+        {/* div with 2 buttons play+my list */}
         <div className="banner-buttons">
           <button className="banner-button">Play</button>
           <button className="banner-button">My List</button>
         </div>
-        {/* div with 2 buttons play+my list */}
         {/* description */}
+
       </div>
 
 
