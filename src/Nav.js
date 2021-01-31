42e37fe721a3e0afaabb183c2506869a9007f764
Barkley Santo <barkley.santo@gmail.com>
2021-01-31 14:48:17 -0500

    useEffect and useState to nav component - adding scroll event listener to add css class to navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Nav.css'
 
 function Nav() {
 
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, [])
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${show && "nav-black"}`}>
       <img
         className="nav-logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
