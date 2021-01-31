7bfcdb13c2d9f3a09c0da305ae7f74ba2d05aa3d
Barkley Santo <barkley.santo@gmail.com>
2021-01-31 14:20:57 -0500

    get netflix logo - insert into Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import './Nav.css'
 
 function Nav() {
 
-  const style = {
-    color: 'white'
-  }
   return (
-    <div style={style}>
-      hello from nav js
+    <div className="navbar">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix logo" />
     </div>
   )
 }
