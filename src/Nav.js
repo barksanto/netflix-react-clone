7bd60547034d917e470bd6c2d6173aacf433da6f
Barkley Santo <barkley.santo@gmail.com>
2021-01-31 14:24:01 -0500

    import avatar logo

--- src/Nav.js
+++ src/Nav.js
@@ -5,8 +5,17 @@ function Nav() {
 
   return (
     <div className="navbar">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix logo" />
+      <img
+        width="50px"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="netflix logo" />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="avatar logo"
+        width="50px" />
+
     </div>
+
   )
 }
 
